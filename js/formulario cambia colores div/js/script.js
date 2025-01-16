eb77aad4f54e4e3f8a9e3160a62c527aae740189
DiegoArmando-hub <diego.a.urgilez@gmail.com>
2025-01-16 18:43:06 +0100

    cambios casa lp1

--- js/formulario cambia colores div/js/script.js
+++ js/formulario cambia colores div/js/script.js
@@ -13,6 +13,11 @@ function enviar(color) {
       c2.style.backgroundColor = "blueviolet";
       c3.style.backgroundColor = "orange";
       break;
+    case "gris":
+      c1.style.backgroundColor = "gainsboro";
+      c2.style.backgroundColor = "darkgray";
+      c3.style.backgroundColor = "gray";
+      break;
   }
 }
 function cambiar(color) {
@@ -23,16 +28,24 @@ function cambiar(color) {
     case "rojo":
       c3.style.backgroundColor = "red";
       c3.innerHTML = "Primero";
-    //   c3.style.gridTemplateColumns="";//continuar revisando
       c2.style.backgroundColor = "yellow";
       c1.style.backgroundColor = "blue";
-      c1.innerHTML="Tercero";
-      //   document.getElementById()
+      c1.innerHTML = "Tercero";
+      document.getElementById("caja").style.gridTemplateColumns = "1fr 1fr 2fr";
       break;
     case "verde":
-      c1.style.backgroundColor = "green";
+      c3.style.backgroundColor = "green";
+      c3.innerHTML = "Primero";
       c2.style.backgroundColor = "blueviolet";
-      c3.style.backgroundColor = "orange";
+      c1.style.backgroundColor = "orange";
+      c1.innerHTML = "Tercero";
+      document.getElementById("caja").style.gridTemplateColumns = "1fr 1fr 2fr";
+      break;
+    case "gris":
+      c3.style.backgroundColor = "gainsboro";
+      c2.style.backgroundColor = "darkgray";
+      c1.style.backgroundColor = "gray";
+      document.getElementById("caja").style.gridTemplateColumns = "1fr 1fr 2fr";
       break;
   }
 }
